f7859930885216df81c5d7c593f74d6895de9171
Enrique1311 <ejspinelli131176@gmail.com>
2024-05-14 19:02:11 -0300

    The disign was completed

--- js/main.js
+++ js/main.js
@@ -103,27 +103,27 @@ const trainers = [
 	{
 		id: "1",
 		name: "Borney Exiteid",
-		trainerImg: "../assets/images/borney-image.png",
+		trainerImg: "../assets/images/trainers-slider/borney-image.png",
 	},
 	{
 		id: "2",
 		name: "Elsa Windia",
-		trainerImg: "../assets/images/elsa-img.png",
+		trainerImg: "../assets/images/trainers-slider/elsa-img.png",
 	},
 	{
 		id: "3",
 		name: "Geourge Aryo",
-		trainerImg: "../assets/images/geourge-img.png",
+		trainerImg: "../assets/images/trainers-slider/geourge-img.png",
 	},
 	{
 		id: "4",
 		name: "Angela Mellisa",
-		trainerImg: "../assets/images/angela-img.jpg",
+		trainerImg: "../assets/images/trainers-slider/angela-img.jpg",
 	},
 ];
 
 function trainersSlider() {
-	// Slider Cards
+	// Trainers Slider Cards
 	const $sliderContent = d.querySelector(".trainers-slider");
 	const $trainersTemplate = d.getElementById(
 		"trainers-slider-template"
@@ -138,25 +138,162 @@ function trainersSlider() {
 		$trainersTemplate
 			.querySelector(".trainers-slider-image")
 			.setAttribute("src", el.trainerImg);
-
 		$trainersTemplate.querySelector("h4").textContent = el.name;
+
 		let $sliderClone = d.importNode($trainersTemplate, true);
 		$slideFragment.appendChild($sliderClone);
 	});
 
 	$sliderContent.appendChild($slideFragment);
 
-	// Slider Buttons
+	// Trainers Slider Buttons
 	const $sliderItem = d.querySelector(".trainers-slider");
 	const $slideButtons = d.querySelectorAll(".slider-btn");
-	console.log($sliderItem, $slideButtons);
 
 	$slideButtons.forEach((btn) => {
 		btn.addEventListener("click", () => {
-			const direction = btn.id === "left-btn" ? -1 : 1;
-			const scrollAmount = ($sliderItem.clientWidth * direction) / 1.75;
+			const direction = btn.id === "left-btn" ? -0.8 : 0.8;
+			const scrollAmount = $sliderItem.clientWidth * direction;
 			$sliderItem.scrollBy({ left: scrollAmount, behavior: "smooth" });
 		});
 	});
 }
 trainersSlider();
+
+// ***********Testimonial Section****************************************************************
+
+const testimonials = [
+	{
+		id: "1",
+		name: "Jhony Breaker",
+		comment:
+			"I am extremely grateful for the positive impact gym training has had on my life; through consistent training, expert guidance from coaches, and access to top-notch facilities, I have witnessed a remarkable transformation in strength, endurance, and overall fitness levels.",
+		stars: "../assets/images/stars.png",
+		userImg: "../assets/images/testimonial-slider/jhony-breaker.jpg",
+	},
+	{
+		id: "2",
+		name: "Cathy Pham",
+		comment:
+			"I am extremely grateful for the positive impact gym training has had on my life; through consistent training, expert guidance from coaches, and access to top-notch facilities, I have witnessed a remarkable transformation in strength, endurance, and overall fitness levels.",
+		stars: "../assets/images/stars.png",
+		userImg: "../assets/images/testimonial-slider/cathy-pham.jpg",
+	},
+	{
+		id: "3",
+		name: "Aaron Brodgen",
+		comment:
+			"I am extremely grateful for the positive impact gym training has had on my life; through consistent training, expert guidance from coaches, and access to top-notch facilities, I have witnessed a remarkable transformation in strength, endurance, and overall fitness levels.",
+		stars: "../assets/images/stars.png",
+		userImg: "../assets/images/testimonial-slider/aaron-brogden.jpg",
+	},
+	{
+		id: "4",
+		name: "Mike Smith",
+		comment:
+			"I am extremely grateful for the positive impact gym training has had on my life; through consistent training, expert guidance from coaches, and access to top-notch facilities, I have witnessed a remarkable transformation in strength, endurance, and overall fitness levels.",
+		stars: "../assets/images/stars.png",
+		userImg: "../assets/images/testimonial-slider/mike-smith.jpg",
+	},
+	{
+		id: "5",
+		name: "Sule Makaroglu",
+		comment:
+			"I am extremely grateful for the positive impact gym training has had on my life; through consistent training, expert guidance from coaches, and access to top-notch facilities, I have witnessed a remarkable transformation in strength, endurance, and overall fitness levels.",
+		stars: "../assets/images/stars.png",
+		userImg: "../assets/images/testimonial-slider/sule-makaroglu.jpg",
+	},
+	{
+		id: "6",
+		name: "Hayley Kim",
+		comment:
+			"I am extremely grateful for the positive impact gym training has had on my life; through consistent training, expert guidance from coaches, and access to top-notch facilities, I have witnessed a remarkable transformation in strength, endurance, and overall fitness levels.",
+		stars: "../assets/images/stars.png",
+		userImg: "../assets/images/testimonial-slider/hayley-kim.jpg",
+	},
+	{
+		id: "7",
+		name: "Jhon Foster",
+		comment:
+			"I am extremely grateful for the positive impact gym training has had on my life; through consistent training, expert guidance from coaches, and access to top-notch facilities, I have witnessed a remarkable transformation in strength, endurance, and overall fitness levels.",
+		stars: "../assets/images/stars.png",
+		userImg: "../assets/images/testimonial-slider/jhon-foster.jpg",
+	},
+	{
+		id: "8",
+		name: "Bruce Mars",
+		comment:
+			"I am extremely grateful for the positive impact gym training has had on my life; through consistent training, expert guidance from coaches, and access to top-notch facilities, I have witnessed a remarkable transformation in strength, endurance, and overall fitness levels.",
+		stars: "../assets/images/stars.png",
+		userImg: "../assets/images/testimonial-slider/bruce-mars.jpg",
+	},
+];
+
+function testimonialSlider() {
+	// Trainers Slider Cards
+	const $testimonialsSliderContent = d.querySelector(".testimonial-slider"),
+		$testimonialsTemplate = d.getElementById(
+			"testimonial-slider-template"
+		).content,
+		$testimonialsFragment = d.createDocumentFragment();
+
+	testimonials.forEach((el) => {
+		$testimonialsTemplate.querySelector("p").textContent = el.comment;
+		$testimonialsTemplate.querySelector(".stars").setAttribute("src", el.stars);
+
+		$testimonialsTemplate.querySelector("h2").textContent = el.name;
+
+		$testimonialsTemplate
+			.querySelector(".testimonial-card-image")
+			.setAttribute("src", el.userImg);
+
+		let $sliderClone = d.importNode($testimonialsTemplate, true);
+		$testimonialsFragment.appendChild($sliderClone);
+	});
+	$testimonialsSliderContent.appendChild($testimonialsFragment);
+}
+
+testimonialSlider();
+
+function moveSlider() {
+	const $testimonialSlider = d.querySelector(".testimonial-slider");
+	let $sliderCards = d.querySelectorAll(".testimonial-slider-card");
+	let lastSlide = $sliderCards[$sliderCards.length - 1];
+	const $btnLeft = d.querySelector(".left-btn-2");
+	const $btnRight = d.querySelector(".right-btn-2");
+
+	console.log($btnRight);
+
+	$testimonialSlider.insertAdjacentElement("afterbegin", lastSlide);
+
+	const nextCard = () => {
+		let firstSlide = d.querySelectorAll(".testimonial-slider-card")[0];
+		$testimonialSlider.style.marginLeft = "-200%";
+		$testimonialSlider.style.transition = "all 0.5s linear";
+
+		setTimeout(() => {
+			$testimonialSlider.style.transition = "none";
+			$testimonialSlider.insertAdjacentElement("beforeend", firstSlide);
+			$testimonialSlider.style.marginLeft = "-100%";
+		}, 500);
+	};
+
+	const prevCard = () => {
+		let $sliderCards = d.querySelectorAll(".testimonial-slider-card");
+		let lastSlide = $sliderCards[$sliderCards.length - 1];
+
+		$testimonialSlider.style.marginLeft = "0";
+		$testimonialSlider.style.transition = "all 0.5s linear";
+
+		setTimeout(() => {
+			$testimonialSlider.style.transition = "none";
+			$testimonialSlider.insertAdjacentElement("afterbegin", lastSlide);
+			$testimonialSlider.style.marginLeft = "-100%";
+		}, 500);
+	};
+
+	$btnRight.addEventListener("click", () => nextCard());
+	$btnLeft.addEventListener("click", () => prevCard());
+}
+
+moveSlider();
